68d5c2783273e96c9abd8e9cfd05a076511b151b
Konrad Lacek <konrad.lacek@outlook.com>
2025-11-18 17:20:01 +0100

    Add project files.

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TwoSumTests")]
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Finds the indices of the two numbers that add up to the target.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        internal static int[] TwoSum(int[] nums, int target)
+        {
+            if (nums.Length < 2)
+            {
+                throw new ArgumentException("Array must contain at least two elements.");
+            }
+            else if (nums.Length == 2)
+            {
+                if (nums[0] + nums[1] == target)
+                {
+                    return [0, 1];
+                }
+                else
+                {
+                    throw new ArgumentException("No two sum solution exists.");
+                }
+            }
+            else
+            {
+                Dictionary<int, int> map = [];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int complement = target - nums[i];
+                    if (map.ContainsKey(complement))
+                    {
+                        return [map[complement], i];
+                    }
+                    map[nums[i]] = i;
+                }
+                throw new ArgumentException("No two sum solution exists.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL, Leet Code Two Sum + Aproppriate Tests");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ TwoSum/TwoSumTests/TwoSumTests.cs
@@ -0,0 +1,60 @@
+﻿using TwoSum;
+
+namespace TwoSumTests
+{
+    public class TwoSumTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] testSet1 = [2, 7, 11, 15];
+            int target1 = 9;
+            int[] correct1 = [0, 1];
+            Assert.Equal(correct1, Program.TwoSum(testSet1, target1));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] testSet2 = [3, 2, 4];
+            int target2 = 6;
+            int[] correct2 = [1, 2];
+            Assert.Equal(correct2, Program.TwoSum(testSet2, target2));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] testSet3 = [3, 3];
+            int target3 = 6;
+            int[] correct3 = [0, 1];
+            Assert.Equal(correct3, Program.TwoSum(testSet3, target3));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] testSet4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            int target4 = 19;
+            int[] correct4 = [8, 9];
+            Assert.Equal(correct4, Program.TwoSum(testSet4, target4));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            int[] testSet5 = [0, -1, 2, -3, 1];
+            int target5 = -2;
+            int[] correct5 = [3, 4];
+            Assert.Equal(correct5, Program.TwoSum(testSet5, target5));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int[] testSet6 = [1, 2, 3];
+            int target6 = 7;
+            Assert.Throws<ArgumentException>(() => Program.TwoSum(testSet6, target6));
+        }
+    }
+}
