13a603e4238540e19cdd30e8563e2e045651c616
Konrad Lacek <konrad.lacek@outlook.com>
2025-11-22 09:39:17 +0100

    LeetCode Longest Substring Without Repetitions + Appropriate Tests

---
+++ LongestSubstring/LongestSubstring/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("LongestSubstringTests")]
+
+namespace LongestSubstring
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Returns the length of the longest substring without repeating characters in the specified string.
+        /// </summary>
+        /// <param name="s">The string to search for the longest substring without repeating characters. Can be null or empty.</param>
+        /// <returns>The length of the longest substring without repeating characters. Returns 0 if the input string is null or
+        /// empty.</returns>
+        internal static int LengthOfLongestSubstring(string s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return 0;
+            }
+            int first = default;
+            int second = default;
+            int maxLength = default;
+            HashSet<char> charSet = new HashSet<char>();
+            while (second < s.Length)
+            {
+                if (!charSet.Contains(s[second]))
+                {
+                    charSet.Add(s[second]);
+                    second++;
+                    maxLength = Math.Max(maxLength, second - first);
+                }
+                else
+                {
+                    charSet.Remove(s[first]);
+                    first++;
+                }
+            }
+            return maxLength;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL LeetCode Longest Substring Without Repetitions - Two Pointer");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ LongestSubstring/LongestSubstringTests/LengthOfLongestSubstringTests.cs
@@ -0,0 +1,43 @@
+﻿using LongestSubstring;
+
+namespace LongestSubstringTests
+{
+    public class LengthOfLongestSubstringTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string testInput = "abcabcbb";
+            int expectedOutput = 3;
+            int actualOutput = Solution.LengthOfLongestSubstring(testInput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string testInput = "bbbbb";
+            int expectedOutput = 1;
+            int actualOutput = Solution.LengthOfLongestSubstring(testInput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string testInput = "pwwkew";
+            int expectedOutput = 3;
+            int actualOutput = Solution.LengthOfLongestSubstring(testInput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            string testInput = "";
+            int expectedOutput = 0;
+            int actualOutput = Solution.LengthOfLongestSubstring(testInput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
