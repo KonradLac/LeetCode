61183aa5c25315117c03c9dac0ec642f1bdc5fa9
KonradLac <konrad.lacek@outlook.com>
2025-11-25 18:21:09 +0100

    LeetCode String to Integer + Appropriate Tests

---
+++ StringToInteger/StringToInteger/Solution.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+[assembly: InternalsVisibleTo("StringToIntegerTests")]
+
+namespace StringToInteger
+{
+    internal class Solution
+    {
+        internal static int MyAtoi(string s)
+        {
+            string trimmed = s.Trim();
+            if (string.IsNullOrEmpty(trimmed))
+            {
+                return 0;
+            }
+            int counter = default;
+            bool isNegative = false;
+            if (trimmed.ToCharArray()[0] == '-')
+            {
+                counter++;
+                isNegative = true;
+            }
+            else if (trimmed.ToCharArray()[0] == '+')
+            {
+                counter++;
+                isNegative = false;
+            }
+            if(trimmed.Length <= counter)
+            {
+                return 0;
+            }
+            bool zeroFound = false;
+            do
+            {
+                if (trimmed[counter].Equals("0"))
+                {
+                    zeroFound = true;
+                    counter++;
+                }
+                else
+                {
+                    zeroFound = false;
+                }
+            } while (zeroFound);
+            long result = default;
+            for (int i = counter; i < trimmed.Length; i++)
+            {
+                char c = (char)trimmed[i];
+                if (!char.IsDigit(c))
+                {
+                    break;
+                }
+                result = result * 10 + (c - '0');
+                if (!isNegative && result > int.MaxValue)
+                {
+                    return int.MaxValue;
+                }
+                if (isNegative && -result < int.MinValue)
+                {
+                    return int.MinValue;
+                }
+            }
+            return isNegative ? (int)-result : (int)result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL LeetCode String to Integer + Appropriate Tests");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Tries & Errors Approach");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/string-to-integer-atoi/");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ StringToInteger/StringToIntegerTests/AtoiTests.cs
@@ -0,0 +1,124 @@
+﻿using StringToInteger;
+
+namespace StringToIntegerTests
+{
+    public class AtoiTests
+    {
+        [Fact]
+        public void StandardTest()
+        {
+            string input = "42";
+            int expected = 42;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void LeadingWhiteSpacesAndNegative()
+        {
+            string input = "   -42";
+            int expected = -42;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void WordsAfterANumber()
+        {
+            string input = "4193 with words";
+            int expected = 4193;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void WordsBeforeANumber()
+        {
+            string input = "words and 987";
+            int expected = 0;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void UnderflowMinValue()
+        {
+            string input = "-91283472332";
+            int expected = int.MinValue;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void LeadingZeros()
+        {
+            string input = "0032";
+            int expected = 32;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void LeadingPlusSign()
+        {
+            string input = "+1";
+            int expected = 1;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void LeadingWhiteSingsAndThenPlusSign()
+        {
+            string input = "   +0 123";
+            int expected = 0;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void NegativeLeadingZeros()
+        {
+            string input = "-000000000000001";
+            int expected = -1;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void OverflowMaxValue()
+        {
+            string input = "2147483648";
+            int expected = int.MaxValue;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void OnlyPlusSign()
+        {
+            string input = "+";
+            int expected = 0;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void OnlyMinusSign()
+        {
+            string input = "-";
+            int expected = 0;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void SuperBigNumber()
+        {
+            string input = "20000000000000000000";
+            int expected = int.MaxValue;
+            int result = Solution.MyAtoi(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
