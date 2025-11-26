a996ed736b4d6ab1bf0508f5335ec808cb0dde4f
KonradLac <konrad.lacek@outlook.com>
2025-11-26 18:19:12 +0100

    LeetCode Palindrome Number + Appropriate Tests

---
+++ PalindromeNumber/PalindromeNumber/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("PalindromeNumberTests")]
+
+namespace PalindromeNumber
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Determines whether the specified integer is a palindrome.
+        /// </summary>
+        /// <remarks>Negative numbers are not considered palindromes.</remarks>
+        /// <param name="x">The integer value to evaluate.</param>
+        /// <returns>true if the value of x reads the same backward as forward; otherwise, false.</returns>
+        internal static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            int originalNumber = x;
+            int revertedNumber = default;
+            while (x != 0)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+            return revertedNumber == originalNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL LeetCode Palindorme Number");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Comparing the reversed number with the original number.");
+            Console.WriteLine("Time Complexity: O(log(n))");
+            Console.WriteLine("Space Complexity: O(1)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/palindrome-number/");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ PalindromeNumber/PalindromeNumberTests/PalindromeTests.cs
@@ -0,0 +1,55 @@
+﻿using static PalindromeNumber.Solution;
+
+namespace PalindromeNumberTests
+{
+    public class PalindromeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int input = 121;
+            bool result = IsPalindrome(input);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int input = -121;
+            bool result = IsPalindrome(input);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int input = 10;
+            bool result = IsPalindrome(input);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int input = -101;
+            bool result = IsPalindrome(input);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            int input = 12321;
+            bool result = IsPalindrome(input);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int input = 0;
+            bool result = IsPalindrome(input);
+            Assert.True(result);
+        }
+    }
+}
