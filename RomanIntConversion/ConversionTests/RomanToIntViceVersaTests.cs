a1fd1b62669486ebd97e62d39dfd020017ec5166
KonradLac <konrad.lacek@outlook.com>
2025-12-28 18:39:49 +0100

    LeetCode Integer To Roman + LeetCode Roman to Integer + Appropriate Tests

---
+++ RomanIntConversion/ConversionTests/RomanToIntViceVersaTests.cs
@@ -0,0 +1,119 @@
+﻿using static RomanIntConversion.Solution;
+
+namespace ConversionTests
+{
+    public class RomanToIntViceVersaTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            int input = 1994;
+            string expectedRoman = "MCMXCIV";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            int input = 58;
+            string expectedRoman = "LVIII";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            int input = 4;
+            string expectedRoman = "IV";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            // Arrange
+            int input = 3999;
+            string expectedRoman = "MMMCMXCIX";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            // Arrange
+            int input = 1;
+            string expectedRoman = "I";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            // Arrange
+            int input = 2763;
+            string expectedRoman = "MMDCCLXIII";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            // Arrange
+            int input = 944;
+            string expectedRoman = "CMXLIV";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            // Arrange
+            int input = 123;
+            string expectedRoman = "CXXIII";
+            // Act
+            string actualRoman = IntToRoman(input);
+            int actualInt = RomanToInt(actualRoman);
+            // Assert
+            Assert.Equal(expectedRoman, actualRoman);
+            Assert.Equal(input, actualInt);
+        }
+    }
+}
---
+++ RomanIntConversion/RomanIntConversion/Solution.cs
@@ -0,0 +1,75 @@
+﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ConversionTests")]
+
+namespace RomanIntConversion
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Converts an integer to its equivalent Roman numeral representation.
+        /// </summary>
+        /// <remarks>Roman numerals are represented using the standard symbols (I, V, X, L, C, D, M). Only
+        /// values from 1 to 3999 are supported; values outside this range may produce incorrect results.</remarks>
+        /// <param name="num">The integer value to convert. Must be in the range 1 to 3999, inclusive.</param>
+        /// <returns>A string containing the Roman numeral representation of the specified integer.</returns>
+        internal static string IntToRoman(int num)
+        {
+            string[] thousands = { "", "M", "MM", "MMM" };
+            string[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] units = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            string romanNumeral = "";
+            romanNumeral += thousands[num / 1000];
+            romanNumeral += hundreds[(num % 1000) / 100];
+            romanNumeral += tens[(num % 100) / 10];
+            romanNumeral += units[num % 10];
+            return romanNumeral;
+        }
+
+        /// <summary>
+        /// Converts a Roman numeral string to its equivalent integer value.
+        /// </summary>
+        /// <remarks>The method assumes that the input string is a well-formed Roman numeral using
+        /// standard notation. Invalid or non-standard Roman numeral strings may produce incorrect results.</remarks>
+        /// <param name="s">A string representing a valid Roman numeral composed of the characters 'I', 'V', 'X', 'L', 'C', 'D', and
+        /// 'M'. The string must not be null or empty.</param>
+        /// <returns>The integer value corresponding to the specified Roman numeral.</returns>
+        internal static int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanMap = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+            int total = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+                {
+                    total -= romanMap[s[i]];
+                }
+                else
+                {
+                    total += romanMap[s[i]];
+                }
+            }
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Roman to Integer and Integer to Roman Conversion");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Tries and errors to get familiar with C#");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/integer-to-roman/");
+            Console.WriteLine(@"https://leetcode.com/problems/roman-to-integer/");
+            Console.ResetColor();
+        }
+    }
+}
