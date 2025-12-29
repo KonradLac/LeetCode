d38f8ca76c4e2544aba6071c3218840a0c0f114b
KonradLac <konrad.lacek@outlook.com>
2025-12-29 08:13:57 +0100

    LeetCode 3Sum Closest + Appropriate Tests

---
+++ ClosestThreeSum/ClosestThreeSum/Solution.cs
@@ -0,0 +1,58 @@
+﻿[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("ClosestThreeSumTests")]
+
+namespace ClosestThreeSum
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Finds the sum of three integers in the array that is closest to the specified target value.</summary>
+        /// <remarks>If multiple sums are equally close to the target, the first one found is returned. The input array is modified by sorting it in place.</remarks>
+        /// <param name="nums">An array of integers to search for the three numbers whose sum is closest to the target. Must contain at least three elements.</param>
+        /// <param name="target">The target sum to approach as closely as possible.</param>
+        /// <returns>The sum of the three integers in the array whose total is closest to the target value.</returns>
+        internal static int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int closestSum = nums[0] + nums[1] + nums[2];
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+                while (left < right)
+                {
+                    int currentSum = nums[i] + nums[left] + nums[right];
+                    if (Math.Abs(currentSum - target) < Math.Abs(closestSum - target))
+                    {
+                        closestSum = currentSum;
+                    }
+                    if (currentSum < target)
+                    {
+                        left++;
+                    }
+                    else if (currentSum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        return currentSum;
+                    }
+                }
+            }
+            return closestSum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL Three Sum Closest + Appropriate Tests");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Two Pointer Approach");
+            Console.WriteLine("Time Complexity: O(n^2)");
+            Console.WriteLine("Space Complexity: O(1)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/3sum-closest/");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ClosestThreeSum/ClosestThreeSumTests/ThreeSumClosestTests.cs
@@ -0,0 +1,47 @@
+﻿using static ClosestThreeSum.Solution;
+
+namespace ClosestThreeSumTests
+{
+    public class ThreeSumClosestTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = { -1, 2, 1, -4 };
+            int target = 1;
+            int expected = 2;
+            int result = ThreeSumClosest(nums, target);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int target = 1;
+            int expected = 6;
+            int result = ThreeSumClosest(nums, target);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = { 1, -2, 3, 4 };
+            int target = 1;
+            int expected = 2;
+            int result = ThreeSumClosest(nums, target);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] nums = { 0, 0, 0 };
+            int target = 1;
+            int expected = 0;
+            int result = ThreeSumClosest(nums, target);
+            Assert.Equal(expected, result);
+        }
+    }
+}
