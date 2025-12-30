87762bb4e0b6eef3d80fb3026cfa35e08dc60b9f
KonradLac <konrad.lacek@outlook.com>
2025-12-30 20:06:04 +0100

    LeetCode Letter Combinations of a phone number + Appropriate tests

---
+++ PhoneLetterCombinations/PhoneLetterCombinations/Solution.cs
@@ -0,0 +1,79 @@
+﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("PhoneLetterCombinationsTests")]
+
+namespace PhoneLetterCombinations
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Generates all possible letter combinations that the input digit string could represent on a standard telephone keypad.</summary>
+        /// <remarks>Each digit maps to a set of letters as follows: '2' to "abc", '3' to "def", '4' to 
+        /// "ghi", '5' to "jkl", '6' to "mno", '7' to "pqrs", '8' to "tuv", and '9' to "wxyz". If the input contains any
+        /// character outside the range '2'-'9', the method returns an empty list.</remarks>
+        /// <param name="digits">A string containing digits from '2' to '9'. Each digit maps to a set of letters as found on a telephone keypad.</param>
+        /// <returns>A list of all possible letter combinations that the input digits could represent. Returns an empty list if 
+        /// the input is null, empty, or contains invalid digits.</returns>
+        internal static IList<string> LetterCombinations(string digits)
+        {
+            if (string.IsNullOrEmpty(digits))
+            {
+                return [];
+            }
+            Dictionary<char, string> phoneMap = new()
+            {
+                { '2', "abc" },
+                { '3', "def" },
+                { '4', "ghi" },
+                { '5', "jkl" },
+                { '6', "mno" },
+                { '7', "pqrs" },
+                { '8', "tuv" },
+                { '9', "wxyz" }
+            };
+            List<string> result = [];
+            foreach (char c in digits)
+            {
+                if (!phoneMap.ContainsKey(c))
+                {
+                    return [];
+                }
+            }
+            for (int i = 0; i < digits.Length; i++)
+            {
+                string currentChars = phoneMap[digits[i]];
+                if (result.Count == 0)
+                {
+                    foreach (char c in currentChars)
+                    {
+                        result.Add(c.ToString());
+                    }
+                }
+                else
+                {
+                    List<string> tempList = [];
+                    foreach (string combination in result)
+                    {
+                        foreach (char c in currentChars)
+                        {
+                            tempList.Add(combination + c);
+                        }
+                    }
+                    result = tempList;
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL Letter Combinations of a phone number + Appropriate Tests");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Brute force");
+            Console.WriteLine("Time complexity: O(n * m), where n is the length of the input digits and m is the average number of letters per digit.");
+            Console.WriteLine("Space complexity: O(k), where k is the number of possible combinations.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/letter-combinations-of-a-phone-number/");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ PhoneLetterCombinations/PhoneLetterCombinationsTests/PhoneTests.cs
@@ -0,0 +1,106 @@
+﻿using static PhoneLetterCombinations.Solution;
+
+namespace PhoneLetterCombinationsTests
+{
+    public class PhoneTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = LetterCombinations("23");
+            List<string> expected =
+            [
+                "ad", "ae", "af",
+                "bd", "be", "bf",
+                "cd", "ce", "cf"
+            ];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var result = LetterCombinations("");
+            List<string> expected = [];
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var result = LetterCombinations("2");
+            List<string> expected = ["a", "b", "c"];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var result = LetterCombinations("9");
+            List<string> expected = ["w", "x", "y", "z"];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var result = LetterCombinations("79");
+            List<string> expected =
+            [
+                "pw", "px", "py", "pz",
+                "qw", "qx", "qy", "qz",
+                "rw", "rx", "ry", "rz",
+                "sw", "sx", "sy", "sz"
+            ];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var result = LetterCombinations("234");
+            List<string> expected =
+            [
+                "adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi",
+                "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi",
+                "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi"
+            ];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            var result = LetterCombinations("1");
+            List<string> expected = [];
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            var result = LetterCombinations("203");
+            List<string> expected = [];
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            var result = LetterCombinations("5678");
+            List<string> expected =
+            [
+                "jmpt", "jmpu", "jmpv", "jmqt", "jmqu", "jmqv", "jmrt", "jmru", "jmrv", "jmst", "jmsu", "jmsv",
+                "jnpt", "jnpu", "jnpv", "jnqt", "jnqu", "jnqv", "jnrt", "jnru", "jnrv", "jnst", "jnsu", "jnsv",
+                "jopt", "jopu", "jopv", "joqt", "joqu", "joqv", "jort", "joru", "jorv", "jost", "josu", "josv",
+                "kmpt", "kmpu", "kmpv", "kmqt", "kmqu", "kmqv", "kmrt", "kmru", "kmrv", "kmst", "kmsu", "kmsv",
+                "knpt", "knpu", "knpv", "knqt", "knqu", "knqv", "knrt", "knru", "knrv", "knst", "knsu", "knsv",
+                "kopt", "kopu", "kopv", "koqt", "koqu", "koqv", "kort", "koru", "korv", "kost", "kosu", "kosv",
+                "lmpt", "lmpu", "lmpv", "lmqt", "lmqu", "lmqv", "lmrt", "lmru", "lmrv", "lmst", "lmsu", "lmsv",
+                "lnpt", "lnpu", "lnpv", "lnqt", "lnqu", "lnqv", "lnrt", "lnru", "lnrv", "lnst", "lnsu", "lnsv",
+                "lopt", "lopu", "lopv", "loqt", "loqu", "loqv", "lort", "loru", "lorv", "lost", "losu", "losv"
+            ];
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+    }
+}
