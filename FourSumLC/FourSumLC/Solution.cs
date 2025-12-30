7e4f29591310cd0ad069795d2f4fa2379cc4c7a4
KonradLac <konrad.lacek@outlook.com>
2025-12-30 20:39:36 +0100

    LeetCode 4Sum + Appropriate Tests

---
+++ FourSumLC/FourSumLC/Solution.cs
@@ -0,0 +1,80 @@
+﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FourSumLCTests")]
+
+namespace FourSumLC
+{
+    internal class Solution
+    {
+        /// <summary>
+        /// Finds all unique quadruplets in the specified array that sum to the given target value.
+        /// </summary>
+        /// <remarks>Each quadruplet in the result is unique; duplicate combinations are not included. The
+        /// order of quadruplets and their elements is not guaranteed.</remarks>
+        /// <param name="nums">An array of integers to search for quadruplets. The array may contain duplicate values.</param>
+        /// <param name="target">The target sum for the quadruplets.</param>
+        /// <returns>A list of lists, where each inner list contains four integers representing a unique quadruplet whose sum
+        /// equals the target. If no quadruplets are found, the list is empty.</returns>
+        internal static IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            long targetLong = (long)target;
+            IList<IList<int>> result = [];
+            int n = nums.Length;
+            for (int i = 0; i < n - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < n - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1])
+                    {
+                        continue;
+                    }
+                    int left = j + 1;
+                    int right = n - 1;
+                    while (left < right)
+                    {
+                        long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == targetLong)
+                        {
+                            result.Add([nums[i], nums[j], nums[left], nums[right]]);
+                            while (left < right && nums[left] == nums[left + 1])
+                            {
+                                left++;
+                            }
+                            while (left < right && nums[right] == nums[right - 1])
+                            {
+                                right--;
+                            }
+                            left++;
+                            right--;
+                        }
+                        else if (sum < targetLong)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("KonradL 4Sum + Appropriate Tests");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Two Pointer Approach, modified 3Sum");
+            Console.WriteLine("Time complexity: O(n^3)");
+            Console.WriteLine("Space complexity: O(1)");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"https://leetcode.com/problems/4sum/");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ FourSumLC/FourSumLCTests/FourSumTests.cs
@@ -0,0 +1,114 @@
+﻿using static FourSumLC.Solution;
+
+namespace FourSumLCTests
+{
+    public class FourSumTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = { 1, 0, -1, 0, -2, 2 };
+            int target = 0;
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -2, -1, 1, 2 },
+                new List<int> { -2, 0, 0, 2 },
+                new List<int> { -1, 0, 0, 1 }
+            };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var list in expected)
+            {
+                Assert.Contains(list, result);
+            }
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = { 1, -1, 0, 0 };
+            int target = 0;
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -1, 0, 0, 1 }
+            };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var list in expected)
+            {
+                Assert.Contains(list, result);
+            }
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = { 0, 0, 0, 0 };
+            int target = 0;
+            var expected = new List<IList<int>>
+            {
+                new List<int> { 0, 0, 0, 0 }
+            };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var list in expected)
+            {
+                Assert.Contains(list, result);
+            }
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] nums = { -3, -1, 0, 2, 4, 5 };
+            int target = 2;
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -3, -1, 2, 4 }
+            };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var list in expected)
+            {
+                Assert.Contains(list, result);
+            }
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int target = 50;
+            var expected = new List<IList<int>> { };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int[] nums = { -5, -4, -3, -2, -1 };
+            int target = -10;
+            var expected = new List<IList<int>>
+            {
+                new List<int> { -4, -3, -2, -1 }
+            };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var list in expected)
+            {
+                Assert.Contains(list, result);
+            }
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            int[] nums = { 1000000000, 1000000000, 1000000000, 1000000000 };
+            int target = -294967296;
+            var expected = new List<IList<int>> { };
+            var result = FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+        }
+    }
+}
