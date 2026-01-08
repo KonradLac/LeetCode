7a431825ddacd0b92f62d20e7880b3276fd52a7a
KonradLac <konrad.lacek@outlook.com>
2026-01-08 10:35:46 +0100

    LeetCode Generate Parentheses + Appropriate Tests

---
+++ ParenthesesGeneration/ParenthesesGenerationTests/ParenthesesValidTests.cs
@@ -0,0 +1,34 @@
+﻿using static ParenthesesGeneration.Solution;
+
+namespace ParenthesesGenerationTests
+{
+    public class ParenthesesValidTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int n = 3;
+            IList<string> expected = new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" };
+            IList<string> result = GenerateParenthesis(n);
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int n = 1;
+            IList<string> expected = new List<string> { "()" };
+            IList<string> result = GenerateParenthesis(n);
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int n = 2;
+            IList<string> expected = new List<string> { "(())", "()()" };
+            IList<string> result = GenerateParenthesis(n);
+            Assert.Equal(expected.OrderBy(x => x), result.OrderBy(x => x));
+        }
+    }
+}
